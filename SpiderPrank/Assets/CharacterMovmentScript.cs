0af3392ffbb42fe9227c489ca94ef5dcdeec6914
Ethan <eitanwass@gmail.com>
2022-01-28 16:55:03 +0200

    Added bounding box wrapping

--- SpiderPrank/Assets/CharacterMovmentScript.cs
+++ SpiderPrank/Assets/CharacterMovmentScript.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class CharacterMovmentScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float speed = 300f;
+
+    private Rigidbody2D rb;
+    [SerializeField] private BoxCollider2D boundingBox;
+
+
+    private void Awake()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Vector2 pos = new Vector2(this.transform.position.x, this.transform.position.y);
+        Vector2 velocity = new Vector2(0, 1);
+
+        velocity *= speed;
+        velocity *= Time.deltaTime;
+
+        rb.velocity = velocity;
+    }
+
+    private void HandleOutOfBounds()
+    {
+        if (rb.position.y > boundingBox.bounds.center.y + boundingBox.bounds.extents.y)
+        {
+            // Out of top
+            rb.position = new Vector2(rb.position.x, boundingBox.bounds.center.y - boundingBox.bounds.extents.y);
+        }
+        if (rb.position.y < boundingBox.bounds.center.y - boundingBox.bounds.extents.y)
+        {
+            // Out of bottom
+            rb.position = new Vector2(rb.position.x, boundingBox.bounds.center.y + boundingBox.bounds.extents.y);
+        }
+        if (rb.position.x > boundingBox.bounds.center.x + boundingBox.bounds.extents.x)
+        {
+            // Out of right
+            rb.position = new Vector2(boundingBox.bounds.center.x - boundingBox.bounds.extents.x, rb.position.y);
+        }
+        if (rb.position.x < boundingBox.bounds.center.x - boundingBox.bounds.extents.x)
+        {
+            // Out of left
+            rb.position = new Vector2(boundingBox.bounds.center.x + boundingBox.bounds.extents.x, rb.position.y);
+        }
     }
 }
