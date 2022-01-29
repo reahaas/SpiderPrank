085ad6aa23a3892b01f50f4ea50019ee135478ea
reahaas <rea@scadafence.com>
2022-01-29 16:21:38 +0200

    Integrate the scoring bord to the game include the actions of kill, new egg and survive

--- SpiderPrank/Assets/Scripts/SpiderEgg.cs
+++ SpiderPrank/Assets/Scripts/SpiderEgg.cs
@@ -15,25 +15,32 @@ public class SpiderEgg : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        ScoreManager.instance.updateEggsOneEgg();
         startTime = Time.realtimeSinceStartup;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!transformed && Time.realtimeSinceStartup - startTime > timeToSpawn)
-        {
-            transformed = true;
-            transform.gameObject.tag = "Enemy";
-            SpriteRenderer spriteR = gameObject.GetComponent<SpriteRenderer>();
-            spriteR.sprite = enemyImage;
-        }
+        if (!this.transformed && Time.realtimeSinceStartup - startTime > timeToSpawn)
+            evolveFromEggToEnemiy();
     }
     
+    private void evolveFromEggToEnemiy()
+    {
+        this.transformed = true;
+        transform.gameObject.tag = "Enemy";
+        SpriteRenderer spriteR = gameObject.GetComponent<SpriteRenderer>();
+        spriteR.sprite = enemyImage;
+
+        ScoreManager.instance.updateIncreaseEnemiesCount();
+    }
+
     void OnTriggerEnter2D(Collider2D col)
     {		
         if (transformed && col.gameObject.tag == "Weapon")
         {
+            ScoreManager.instance.updateKillsOneKill();
             Destroy(this.gameObject);
         }
     }
